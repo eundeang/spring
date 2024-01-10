9b735ba3085e3d0126b53f1028e318070e7d74e6
eundeang <111877048+eundeang@users.noreply.github.com>
2024-01-11 04:20:07 +0900

    feat: 회원 도메인

---
+++ src/main/java/org/example/springboot_begin/MemberApp.java
@@ -0,0 +1,18 @@
+package org.example.springboot_begin;
+
+import member.Grade;
+import member.Member;
+import member.MemberService;
+import member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
