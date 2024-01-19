a6d8b029e9ccf735cdaa859cf70b6a389ed88343
eundeang <111877048+eundeang@users.noreply.github.com>
2024-01-20 06:11:36 +0900

    스프링 빈 조회-동일한 타입이 둘 이상

---
+++ src/test/java/org/example/springboot_begin/beanfind/ApplicationContextSameBeanFindTest.java
@@ -0,0 +1,62 @@
+package org.example.springboot_begin.beanfind;
+
+import member.MemberRepository;
+import member.MemoryMemberRepository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextSameBeanFindTest {
+    AnnotationConfigApplicationContext ac  = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다.")
+    void findBeanByTypeDuplicate() {
+        MemberRepository bean = ac.getBean(MemberRepository.class);
+        assertThrows(NoUniqueBeanDefinitionException.class,
+                () -> ac.getBean(MemberRepository.class));
+    }
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 빈 이름을 지정하면 됨")
+    void findBeanByName() {
+        // 컨테이너 박스 안에 있는 memberRepository1을 지정
+        MemberRepository memberRepository = ac.getBean("memberRepository1",MemberRepository.class);
+        assertThat(memberRepository).isInstanceOf(MemberRepository.class);
+    }
+
+    // 나중에 자동으로 적용되는 기능이여서 알아두어야 함
+    @Test
+    @DisplayName("특정 타입을 모두 조회하기")
+    void findAllBeansByType() {
+        Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value = " + beansOfType.get(key));
+        }
+        System.out.println("beansOfType = " + beansOfType);
+        assertThat(beansOfType.size()).isEqualTo(2);
+
+    }
+    @Configuration
+    static class SameBeanConfig {
+
+        @Bean
+        public MemberRepository memberRepository1() {
+            return new MemoryMemberRepository();
+        }
+
+        @Bean
+        public MemberRepository memberRepository2() {
+            return new MemoryMemberRepository();
+        }
+
+    }
+
+}
